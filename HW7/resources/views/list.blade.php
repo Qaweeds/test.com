7c55ead8eb796c78af8f4f30f2510d07a00bcaec
ram2 <qaweeds@gmail.com>
2022-10-15 16:32:23 +0300

    HW7

--- HW7/resources/views/list.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container" class="mt-10">
-    <ul class="list-group">
-        @foreach($links as $link)
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                <a href="{{ $link['link'] }}">{{ $link['name'] }}</a>
-            </li>
-        @endforeach
-    </ul>
-</div>
